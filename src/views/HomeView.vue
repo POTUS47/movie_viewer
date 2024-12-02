fbdce755123e657d24113fcbcd1e0c91494be9e7
POTUS47 <111119326+POTUS47@users.noreply.github.com>
2024-12-02 15:54:15 +0800

    一个示例程序

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,21 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { ipcRenderer } from 'electron';
+const router = useRouter()
+function jump_test() {
+  router.push('/test')
+}
+function minimize_win() {
+  ipcRenderer.send("minWin")
+}
+</script>
+
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="jump_test">jump test</button>
+    <button @click="minimize_win">aa</button>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: false,
+  configureWebpack: {
+    externals: {
+      'electron': 'require("electron")'
+    },
+  },
 })
